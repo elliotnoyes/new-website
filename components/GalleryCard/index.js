66f1457793b156d40f67ccc6b2bdd95a6f86a608
Henry Noyes <noyes.he@northeastern.edu>
2024-05-01 12:51:36 -0400

    disable modal for now

--- components/GalleryCard/index.js
+++ components/GalleryCard/index.js
@@ -1,30 +1,15 @@
-import React, { useState }from "react";
+import React from "react";
 import Image from "next/image";
-import Button from "../Button";
-import Link from "next/link";
-import { useTheme } from "next-themes";
 
 const GalleryCard = ({ img, AR_src, w_disp, h_disp, name}) => {
 
-  const [isModalOpen, setModalOpen] = useState(false);
-  const { theme } = useTheme();
-
-
-  const openModal = () => {
-    setModalOpen(true);
-  };
-
-  const closeModal = () => {
-    setModalOpen(false);
-  };
-  
   return (
     <div
       className="p-2 first:ml-0"
     >
       <div
         className="rounded-lg overflow-hidden relative"
-        onClick={openModal}
+        // onClick={openModal}
       >
         <Image
           alt={name}
@@ -36,26 +21,6 @@ const GalleryCard = ({ img, AR_src, w_disp, h_disp, name}) => {
         ></Image>
       </div>
 
-      {isModalOpen && (
-        <div className="modal">
-          <div className="modal-content">
-            <div className="flex-row">
-              <Button type="cancel" onClick={closeModal}>
-                <Image
-                  alt="close"
-                  src="/images/header/cancel-white.svg"
-                  width={32}
-                  height={32}
-                ></Image>
-              </Button>
-              <Image alt={name} src={img} 
-              width={(window.innerWidth < 768 ? window.innerHeight : window.innerWidth) * 0.35 * AR_src}
-              height={(window.innerWidth < 768 ? window.innerHeight : window.innerWidth) * 0.35}/>
-            </div>
-          </div>
-        </div>
-      )}
-    
     </div>
   );
 };
